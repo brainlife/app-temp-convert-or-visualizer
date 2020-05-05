be49cb745e9938462e47a28967b440ae479dbe37
Brad Caron <bacaron@iu.edu>
2020-05-04 21:24:07 -0400

    Reason: add compile script

---
+++ compile.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash
+module load matlab/2017a
+
+mkdir compiled
+
+cat > build.m <<END
+addpath(genpath('/N/u/brlife/git/jsonlab'))
+mcc -m -R -nodisplay -d compiled main
+exit
+END
+matlab -nodisplay -nosplash -r build
