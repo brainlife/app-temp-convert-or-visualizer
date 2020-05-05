97a304d23536ecad8e242e00c0ebd26916ab5545
Bradley Caron <bacaron@h1.carbonate.uits.iu.edu>
2020-05-04 21:53:11 -0400

    recompile

--- main.m
+++ main.m
@@ -3,14 +3,14 @@ function [] = main()
 if ~isdeployed
     disp('loading path')
     %for IU HPC
-    addpath(genpath('/N/u/brlife/wma_tools/Utils/External/'))
+    addpath(genpath('/N/u/brlife/wma_tools'))
     addpath(genpath('/N/u/brlife/git/vistasoft'))
     addpath(genpath('/N/u/brlife/git/jsonlab'))
 
     %for old VM
     addpath(genpath('/usr/local/vistasoft'))
     addpath(genpath('/usr/local/jsonlab'))
-    addpath(genpath('/usr/local/wma_tools/Utils/External/'))
+    addpath(genpath('/usr/local/wma_tools'))
 end
 
 % Set top directory
